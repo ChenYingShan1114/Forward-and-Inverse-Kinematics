28c668ec781ab4ced87ff6a11966644f22d7d177
nchc <cindy1999881114@gmail.com>
2026-04-14 10:06:34 +0800

    fixbug/weight_index

--- src/linear_blend_skinning.cpp
+++ src/linear_blend_skinning.cpp
@@ -11,6 +11,7 @@ void linear_blend_skinning(
   // Replace with your code
   
   for (int i = 0; i < V.rows(); i++) { // which vertex
+    int w_index = 0;
 
     Eigen::Vector3d V_i = V.row(i);
 
@@ -18,9 +19,10 @@ void linear_blend_skinning(
     Eigen::Vector3d V_new = Eigen::Vector3d::Zero();
     for (int j = 0; j < skeleton.size(); j++) { // which bone
       if (skeleton[j].weight_index != -1) {
-        double weight = W(i, j-1); // weight for vertex i by bone j-1: W.cols() = skeleton.size() - 1
+        double weight = W(i, w_index); // weight for vertex i by bone w_index: W.cols() = skeleton.size() - # (skeleton[i].weight_index == -1)
         Eigen::Vector3d V_tmp = T[j] * V_i;
         V_new += weight * V_tmp;
+        w_index++;
       }
     }
     U.row(i) = V_new;
